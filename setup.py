79ed4993c8fe5b728c6d0cc64799c9bb36aa8639
Andrey Churin <a.churin@qiwi.com>
2018-02-19 09:47:45 +0300

    v0.3.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='apistar-peewee',
-    version='0.3.1',
+    version='0.3.2',
     url='https://github.com/aachurin/apistar_peewee',
     license='BSD',
     author='Andrey Churin',
